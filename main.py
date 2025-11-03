27cf64c8d756211dec9810fbb5bb18248a048167
Szymon Kubica <szymon.kubica@srccode.pl>
2025-11-03 08:56:05 +0100

    preprocess

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import re
 import scipy.io
 import os
 import csv
+import matplotlib.pyplot as plt
+from collections import Counter
 
 def fill_diseases(path):
     diseases = {}
@@ -18,18 +21,31 @@ if __name__ == '__main__':
 
     directory = r"example"
 
+    # preprocess results as pairs of 0 - disease, 1 - ecg data
+    results = []
+
     for path, _, files in os.walk(directory):
         for filename in files:
             if filename.endswith(".hea"):
                 with open(os.path.join(path, filename), "r") as f:
                     data = f.read()
+                    match = re.search(r"#Dx:\s*([0-9,]+)", data)
+                    if match:
+                        dx_codes = match.group(1).split(',')
 
                 mat_filename = os.path.join(path, os.path.splitext(filename)[0] + ".mat")
                 if os.path.isfile(mat_filename):
                     mat = scipy.io.loadmat(mat_filename)
-                    a = 3
+                    results.append((dx_codes, mat))
 
+    all_diseases = []
+    for diseases, data in results:
+        all_diseases.extend(diseases)
 
+    diseases_counts = Counter(all_diseases)
+    plt.bar(diseases_counts.keys(), diseases_counts.values(), color='skyblue', edgecolor='black')
+    plt.show()
+    a = 3
 
 
 
